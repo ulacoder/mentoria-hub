1128d9a622cb78f9b52047717c0f7eb1199061d2
Mentoria Hub <ulagatnurtas10@gmail.com>
2026-06-18 22:04:57 +0500

    fix: restore GlobalAuthModal - IT WAS NEEDED

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { LocaleProvider } from "@/contexts/locale-context";
 import { NaviMentor } from "@/components/navi-mentor";
 import { Navbar } from "@/components/navbar";
 import { CalendarWidget } from "@/components/calendar-widget";
+import { GlobalAuthModal } from "@/components/global-auth-modal";
 
 const spaceGrotesk = Space_Grotesk({
   variable: "--font-heading",
@@ -48,6 +49,7 @@ export default function RootLayout({
             </main>
             <NaviMentor />
             <CalendarWidget />
+            <GlobalAuthModal />
           </AuthProvider>
         </LocaleProvider>
       </body>
